0a61bf853249b9b2de4ff5a34a999d00fd6b40c1
William Moses <wmoses5k@gmail.com>
2022-10-13 16:29:38 +0100

    Added Ground and changed character to prefabs

---
+++ 3232-Coursework/Assets/Scripts/CharacterAttributes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAttributes : MonoBehaviour
+{
+    public int maxHP;
+    public int currentHP;
+
+    public int damage;
+
+}

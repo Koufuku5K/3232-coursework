2584b77dfb22db7f234ad8fa9b8f8d32e30c51d1
William Moses <wmoses5k@gmail.com>
2022-10-25 13:49:11 +0100

    Added Trigger on bolt
    Still not working

--- 3232-Coursework/Assets/Scripts/Bolt.cs
+++ 3232-Coursework/Assets/Scripts/Bolt.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class Bolt : MonoBehaviour
 {
+    public Rigidbody2D rb;
+
+    CharacterAttributes player;
+    EnemyAttributes enemy;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.velocity = new Vector2(20f, 0f);
     }
 
     public void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.tag == "Enemy")
         {
-            // Damage the enemy
+            // Damage Enemy
             // Destroy the bolt
         }
     }

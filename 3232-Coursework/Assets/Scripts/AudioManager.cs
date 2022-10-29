2cabe9db5ea90abc107bba4040353f388f555fe6
William Moses <wmoses5k@gmail.com>
2022-10-29 21:42:35 +0100

    Added BGM

--- 3232-Coursework/Assets/Scripts/AudioManager.cs
+++ 3232-Coursework/Assets/Scripts/AudioManager.cs
@@ -1,13 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    public AudioSource limitSFX;
+    public Sounds[] sounds;
 
-    public void playLimitSFX()
+    void Awake()
     {
-        limitSFX.Play();
+        foreach (Sounds s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Start()
+    {
+        Play("BGM");
+    }
+
+    public void Play(string name)
+    {
+        Sounds s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # 3232-coursework
 A small game for 3232 coursework.
-Test
+
+Credits:
+
+- Background Music: Boss Time by David Renda.
+Credit to https://www.FesliyanStudios.com for the background music.
